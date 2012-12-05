8690505aba3ff3f0e8047002534bf5ef13f0896d
lasse christiansen <webdevlasse@gmail.com>
2012-12-05 11:03:39 -0800

    Update app/controllers/questions_controller.rb

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,86 +4,55 @@ class QuestionsController < ApplicationController
 
    def index
       @questions = @poll.questions
+   end
 
-      respond_to do |format|
-        format.html # index.html.erb
-        format.json { render json: @questions }
-      end
-    end
-
-
-
-    # GET /polls/1
-    # GET /polls/1.json
-    def show
+   def show
       @question = Question.find(params[:id])
+   end
 
-      respond_to do |format|
-        format.html  #{ redirect_to poll_question_path(@poll) }
-        format.json { render json: @question }
-      end
-    end
 
-    # GET /polls/new
-    # GET /polls/new.json
     def new
       @question = Question.new
       3.times {@question.answers.build}
       # :notice => "The encrypted url(slug) for your poll is #{@question.slug}"
-#       flash[:notice] => "The encrypted url(slug) for your poll is #{@answer.slug}",
-      respond_to do |format|
-        format.html # new.html.erb
-        format.json { render json: @question }
-      end
+      #flash[:notice] => "The encrypted url(slug) for your poll is #{@answer.slug}",
     end
 
-    # GET /polls/1/edit
     def edit
       @question = @poll.questions.find(params[:id])
     end
 
-    # POST /polls
-    # POST /polls.json
+
     def create
       @question = @poll.questions.new(params[:question])
 
       respond_to do |format|
         if @question.save
-
           format.html {  redirect_to poll_questions_path(@poll) }
-          format.json { render json: @question, status: :created, location: @question }
         else
-          format.html { render action: "new" }
-          format.json { render json: @question.errors, status: :unprocessable_entity }
+          format.html { render action: "new" 
         end
       end
     end
 
-    # PUT /polls/1
-    # PUT /polls/1.json
     def update
       @question = @poll.questions.find(params[:id])
 
       respond_to do |format|
         if @question.update_attributes(params[:question])
           format.html { redirect_to poll_questions_path(@poll)  }
-          format.json { head :no_content }
         else
           format.html { render action: "edit" }
-          format.json { render json: @question.errors, status: :unprocessable_entity }
         end
       end
     end
 
-    # DELETE /polls/1
-    # DELETE /polls/1.json
     def destroy
       @question = @poll.questions.find(params[:id])
       @question.destroy
 
       respond_to do |format|
         format.html { redirect_to poll_questions_path(@poll) }
-        format.json { head :no_content }
       end
     end
 
